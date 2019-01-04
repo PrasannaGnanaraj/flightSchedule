51d803a86618a6ae28cbec1d4e803e9471245a7f
Prasanna Gnanaraj <prasanna.gnanaraj@alphasights.com>
2019-01-04 16:19:34 +0000

    aircraft details vue

---
+++ src/components/airCraftDetails/airCraftDetails.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <tableComponent :rows="aircraftDetailsList" :headers="airCraftDetailsProperties" uniqueId='id'>
+            
+        </tableComponent>
+    </div>
+</template>
+<script>
+import tableComponent from '@/components/table/table.vue'
+import { mapGetters } from 'vuex';
+export default {
+    computed:{
+...mapGetters({aircraftDetailsList:'getaircraftDetailsList',airCraftDetailsProperties:'getaircraftDetailsListProperties'})
+  },
+}
+</script>
