bfcb25ab2bc316945a9c7a33032ee794bbb03b11
Prasanna Gnanaraj <prasanna.gnanaraj@alphasights.com>
2019-01-04 16:20:12 +0000

    including edit flight modal

--- src/components/flightList/flightList.js
+++ src/components/flightList/flightList.js
@@ -1,21 +1,29 @@
 
 import tableComponent from '@/components/table/table.vue'
 import { mapGetters, mapActions } from 'vuex';
+import editModal from '@/components/editFlightDetails/editFlightDetails.vue'
 
 export default {
   data() {
     return {
-      msg:'hey'
+      msg:'hey',
+      selectedFlight:null,
+      showEditModal:false
     };
   },
   computed:{
 ...mapGetters({flightList:'getFlightList',flightListProperties:'getFlightListProperties'})
   },
   components:{
-    tableComponent
+    tableComponent,
+    editModal
   },
   methods: {
-      ...mapActions(['fetchFlights'])
+      ...mapActions(['fetchFlights']),
+      handleEditRequest(uniqueId){
+          this.selectedFlight = this.flightList.find((flight)=>flight.ident===uniqueId)
+          this.showEditModal = true;
+      }
   },
   mounted() {
       this.fetchFlights();
--- src/components/flightList/flightList.vue
+++ src/components/flightList/flightList.vue
@@ -1,5 +1,10 @@
 <template>
-    <tableComponent :rows="flightList" :headers="flightListProperties" uniqueId='ident'></tableComponent>
+    <div>
+        <h3>List of Flights Planned for today</h3>
+    <tableComponent :rows="flightList" :headers="flightListProperties" uniqueId='ident' @edit="handleEditRequest"></tableComponent>
+    <editModal></editModal>
+    </div>
+    
 </template>
 <script src='./flightList.js'>
 </script>
