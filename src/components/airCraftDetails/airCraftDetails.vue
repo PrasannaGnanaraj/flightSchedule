daf8566d499b879140108f17a9e84ea52416b42a
PrasannaDnae <gnanaprasanna.gnanaraj@gmail.com>
2019-01-05 14:45:28 +0000

    aircraft details componenet added

--- src/components/airCraftDetails/airCraftDetails.vue
+++ src/components/airCraftDetails/airCraftDetails.vue
@@ -1,14 +1,40 @@
 <template>
-    <div>
-    
+  <div>
+    <h5>Available Aircrafts</h5>
+    <div
+      class="card"
+      v-for="aircraft in aircraftList"
+      v-bind:key="aircraft.ident"
+      style="width:10em"
+    >
+      <div class="card-body">
+        <h5 class="card-title">{{aircraft.ident}}</h5>
+        <div class="d-flex flex-row bd-highlight mb-3" style="font-size:0.7em">
+          <div class="p-2 bd-highlight">
+            <b class="d-block">Type</b>
+            {{aircraft.type}}
+          </div>
+          <div class="p-2 bd-highlight">
+            <b class="d-block">Base</b>
+            {{aircraft.base}}
+          </div>
+          <div class="p-2 bd-highlight">
+            <b class="d-block">Seats</b>
+            {{aircraft.economySeats}}
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
-import tableComponent from '@/components/table/table.vue'
-import { mapGetters } from 'vuex';
+import { mapGetters } from "vuex";
 export default {
-    computed:{
-...mapGetters({totalFlyingTime:'getTotalFlyingTime'})
-  },
-}
+  computed: {
+    ...mapGetters({
+      totalFlyingTime: "getTotalFlyingTime",
+      aircraftList: "getAircraftList"
+    })
+  }
+};
 </script>
