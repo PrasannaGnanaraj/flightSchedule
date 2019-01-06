23bced9478b576ebff2220901503ffcdcae5cb49
PrasannaDnae <gnanaprasanna.gnanaraj@gmail.com>
2019-01-06 17:51:03 +0000

    importing icons

--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import router from './router';
-import Axios from 'axios';
-import store from './store';
+import Vue from "vue";
+import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.css";
+import router from "./router";
+import Axios from "axios";
+import store from "./store";
 
-Vue.config.productionTip = false
-Axios.defaults.headers.common.Accept = 'application/json';
+Vue.config.productionTip = false;
+Axios.defaults.headers.common.Accept = "application/json";
 Axios.defaults.baseURL = "https://infinite-dawn-93085.herokuapp.com";
-
 
 new Vue({
   render: h => h(App),
   router,
-  store,
-}).$mount('#app')
+  store
+}).$mount("#app");
